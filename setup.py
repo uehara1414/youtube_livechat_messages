ea76bd732c80de1851763754421326deeff3ec02
uehara1414 <b1015062@fun.ac.jp>
2018-09-14 11:43:22 +0900

    Add install_requires

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(name='youtube_livechat_messages',
       author='uehara1414',
       author_email='akiya.noface@gmail.com',
       url='https://github.com/uehara1414/youtube_livechat_messages',
+      install_requires=[
+            'oauth2client',
+            'httplib2',
+            'requests',
+      ],
       packages=find_packages(),
       )
